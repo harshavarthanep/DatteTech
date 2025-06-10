26e158f52b45be93ed7148fa4aabfcb3d68a7b25
harshavarthanep <113576943+harshavarthanep@users.noreply.github.com>
2025-06-10 11:49:49 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,7 @@ const config: Config = {
       banner2: "url(/assets/img/hero/banner2.png)",
       banner3: "url(/assets/img/hero/banner3.png)",
       banner4: "url(/assets/img/hero/banner4.png)",
+      banner5: "url(/assets/img/hero/banner5.png)",
       goku: "url(/assets/img/hero/goku.svg)",
     },
     extend: {
