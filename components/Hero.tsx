c7df0dfd66f31692873f4e81bd555d337a99eef7
harshavarthanep <113576943+harshavarthanep@users.noreply.github.com>
2025-06-10 08:59:32 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="h-[80vh] lg:h-[912px] bg-Wbanner1 bg-cover bg-center bg-no-repeat"
+      className="h-[80vh] lg:h-[912px] bg-goku bg-cover bg-center bg-no-repeat"
     >
       <div className="container mx-auto h-full">
         {/* slider */}
