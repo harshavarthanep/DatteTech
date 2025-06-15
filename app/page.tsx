a3555ff3cbb2da69096e48331e1c6d5e1cfbbc03
harshavarthanep <113576943+harshavarthanep@users.noreply.github.com>
2025-06-15 23:38:57 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <WhatWeDo />
       <WhoWeAre />
       <WhyDatteTech />
-      <Blog />
+{/*       <Blog /> */}
       <Team />
       <Testimonial />
     </main>
