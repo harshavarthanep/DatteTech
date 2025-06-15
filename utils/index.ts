f5d27ab148333749dbc49ff04c43e8793e3305d8
harshavarthanep <113576943+harshavarthanep@users.noreply.github.com>
2025-06-15 23:36:40 +0530

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ export const links = [
   { name: "what we do", target: "whatWeDo", offset: -80 },
   { name: "who we are", target: "whoWeAre", offset: -80 },
   { name: "why DatteTech", target: "whyDatteTech", offset: -80 },
-  { name: "blog", target: "blog", offset: -80 },
+  // { name: "blog", target: "blog", offset: -80 },
   { name: "leaders", target: "team", offset: -80 },
   { name: "testimonial", target: "testimonial", offset: -80 },
   { name: "contact", target: "contact", offset: -70 },
