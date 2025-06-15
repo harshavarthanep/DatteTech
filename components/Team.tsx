faed10576cb404e1a18c38ad547718873ed8abaa
harshavarthanep <113576943+harshavarthanep@users.noreply.github.com>
2025-06-15 14:02:24 +0530

    Update Team.tsx

--- components/Team.tsx
+++ components/Team.tsx
@@ -6,17 +6,18 @@ import Image from "next/image";
 
 const trainerData = [
   {
-    image: "/assets/img/trainers/david.jpg",
+    image: "/assets/img/trainers/Harsha1.jpg",
     name: "Harsha Varthan E P",
     role: "Founder & CEO",
-    description: "Passionate enterprenur who has experimented his life with lots of tech",
+    description: 
+      "A true tech polymath and passionate entrepreneur, Harsha has a deep-seated love for transforming ideas into reality. As the force behind H24 Creationz and the author of two organically successful books (500+ copies sold in 7 months), he's built an impressive portfolio: over 6 apps and 2 games on Play Store, 10+ websites, 5+ branding projects, 4+ YouTube channels, 5+ music albums across major platforms, and a self-published podcast. Harsha's vast hands-on experience, including navigating numerous challenges and failures, uniquely positions him to guide aspiring individuals and businesses. He excels at providing strategic insights and building brands from the ground up, making him an invaluable ally for anyone looking to grow in the tech landscape.",
   },
-  // {
-  //   image: "/assets/img/trainers/matt.jpg",
-  //   name: "Matt",
-  //   role: "Body builder coach",
-  //   description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-  // },
+  {
+    image: "/assets/img/trainers/matt.jpg",
+    name: "Vidhya V",
+    role: "Founder & Head of Talent Growth",
+    description: "Vidhya brings extensive expertise in Human Resources and career guidance to DatteTech. With a proven track record of conducting numerous impactful career guidance sessions in colleges, she understands the nuances of talent development and acquisition. Vidhya is dedicated to helping businesses build strong teams by providing comprehensive HR services, from strategic hiring to fostering a thriving workplace culture. Her insights are crucial in ensuring DatteTech's clients can attract, retain, and develop the right talent to achieve their goals.",
+  },
 ];
 
 const Team = () => {
@@ -51,14 +52,14 @@ const Team = () => {
                 key={index}
               >
                 {/* image */}
-{/*                 <div className="relative w-[250px] h-[280px] sm:w-[300px] sm:h-[330px] lg:w-[350px] lg:h-[390px] mx-auto mb-4">
+                <div className="relative w-[250px] h-[280px] sm:w-[300px] sm:h-[330px] lg:w-[350px] lg:h-[390px] mx-auto mb-4">
                   <Image
                     src={trainer.image}
                     fill
                     alt={trainer.name}
                     className="rounded-lg object-cover"
                   />
-                </div> */}
+                </div>
                 <h4 className="text-lg md:text-xl font-semibold mb-2">
                   {trainer.name}
                 </h4>
